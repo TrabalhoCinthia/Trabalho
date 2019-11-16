a60788e5031e2c8f08eec6c64bb2aa17de906f92
bruno-br <43356485+bruno-br@users.noreply.github.com>
2019-11-16 20:01:46 -0300

    Update ArvoreAVL.java

--- Trabalho/ArvoreAVL.java
+++ Trabalho/ArvoreAVL.java
@@ -1,20 +1,21 @@
 package Trabalho;
 /*
 	---------------------------------------- 
-essa classe deve implementar os numeros 18 ao 22 no trabalho; 
+FaÃ§a os itens de 18 a 22 para cada um dos tamanhos (500, 1000, 5000, 10000 e
+50000), para cada tipo de arquivo (aleatÃ³rio, ordenado e invertido), usando AVL. 
 ----------------------------------------
 	18) Comece a contar o tempo.
 
 	19) Carregue uma AVL com os registros de um dos arquivos, tendo como chave o CPF.
-		Balanceie a árvore. Cuidado com os CPFs iguais: vocês devem pensar em uma
+		Balanceie a Ã¡rvore. Cuidado com os CPFs iguais: vocÃªs devem pensar em uma
 		maneira eficiente de armazenar os CPFs iguais
 	
-	20) Faça a pesquisa na AVL, usando os 400 registros fornecidos pela professora, gerando
+	20) FaÃ§a a pesquisa na AVL, usando os 400 registros fornecidos pela professora, gerando
 		arquivos no mesmo modelo do item (3).
 	
 	21) Repita 4 vezes os processos 19 e 20
 
-	22) Termine de contar o tempo, faça uma média e armazene este resultado
+	22) Termine de contar o tempo, faÃ§a uma mÃ©dia e armazene este resultado
 */
 
 

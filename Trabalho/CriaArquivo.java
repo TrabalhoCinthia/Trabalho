31e5369a7d74798e76f760f16c28b87f5a7535c3
bruno-br <43356485+bruno-br@users.noreply.github.com>
2019-11-16 19:38:20 -0300

    Update CriaArquivo.java

--- Trabalho/CriaArquivo.java
+++ Trabalho/CriaArquivo.java
@@ -1,17 +1,29 @@
 package Trabalho;
 
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 
 public class CriaArquivo {
 	
+	//Padrao de título: (HEAP)+tipo(ALEA ou ORD ou INV)+ tamanho.txt
 	public static void criaTxt(String titulo, String conteudo) {
 		File f = new File("src/Arquivos/"+titulo);
+		FileWriter escreve = null;
+		
 		try {
-			f.createNewFile();
+			escreve = new FileWriter(f);
+			escreve.write(conteudo);
 		} catch (IOException e) {
 			e.printStackTrace();
+		}finally {
+			try {
+				escreve.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 		}
+		
 	}
 	
 }

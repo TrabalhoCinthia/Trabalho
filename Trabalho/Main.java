c69815e85cf7fc4e9de649afc31a522bfffc9b33
bruno-br <43356485+bruno-br@users.noreply.github.com>
2019-11-16 20:18:03 -0300

    Update Main.java

--- Trabalho/Main.java
+++ Trabalho/Main.java
@@ -1,25 +1,9 @@
 package Trabalho;
 /*
 ----------------------------------------- 
-  Essa classe é responsavel por chamar as outras. 
+  Essa classe Ã© responsavel por chamar as outras. 
 -----------------------------------------
 */
-/*
-	---------------------------------------- 
-essa classe deve implementar os numeros 13 ao 17 no trabalho; 
-----------------------------------------
-13) Comece a contar o tempo.
-
-14) Carregue uma ABB com os registros de um dos arquivos, tendo como chave o CPF.
-	Balanceie a árvore. Cuidado com os CPFs iguais: vocês devem pensar em uma
-	maneira eficiente de armazenar os CPFs iguais
-
-15) Faça a pesquisa na ABB, usando os 400 registros fornecidos pela professora,
-	gerando arquivos no mesmo modelo do item (3).
-
-16) Repita 4 vezes os processos 14 e 15
-
-17) Termine de contar o tempo, faça uma média e armazene este resultado
 */
 public class Main {
 
@@ -27,26 +11,43 @@ public class Main {
 		
 	}
 	
+	//Ex 13 a 17
 	private void ex13a17() {
 		//13
 		long inicio_milis = System.currentTimeMillis();
 		long final_milis;
+		long media = 0;
+		long[] cpfs;
+		long[] Clientes400 = new long[100];// <- Trocar
 		
-		//14
-		long[] cpfs = new long[100];// <--- Trocar
-		ArvoreABB arv = new ArvoreABB();
-		for(long cpf : cpfs) {
-			Item item = new Item(cpf);
-			arv.insere(item);
-		}
-		
-		//15
-		for(long cpf : cpfs) {
+		//16
+		for(int vez = 1; vez <= 4; vez++) {
 			
+			cpfs = new long[100];// <- Trocar para cada vetor (500, 1000, 5000, 10000, ...)
+			
+			//14
+			ArvoreABB arv = new ArvoreABB();
+			for(long cpf : cpfs) {
+				Item item = new Item(cpf);
+				arv.insere(item);
+			}
+			
+			//15
+			String arquivo = "";
+			for(long cpf : Clientes400) {
+				if(arv.pesquisa(cpf))
+					arquivo += cpf+"\n";
+			}
+			CriaArquivo.criaTxt("(ABB)"+tipo+tamanho+".txt", arquivo);
+			
+			final_milis = System.currentTimeMillis();
+			media += final_milis-inicio_milis;
 		}
 		
+		//17
+		media = media/4;
 		
-		
+		//armazenar a media
 		
 	}
 	

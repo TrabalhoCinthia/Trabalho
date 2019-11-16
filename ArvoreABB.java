cafd08b5dd6abebad98024d85780622b7d7184ed
bruno-br <43356485+bruno-br@users.noreply.github.com>
2019-11-16 17:02:29 -0300

    Add files via upload

---
+++ ArvoreABB.java
@@ -0,0 +1,114 @@
+package Trabalho;
+/*
+ 	---------------------------------------- 
+	essa classe deve implementar os numeros 13 ao 17 no trabalho; 
+	----------------------------------------
+	13) Comece a contar o tempo.
+	
+	14) Carregue uma ABB com os registros de um dos arquivos, tendo como chave o CPF.
+		Balanceie a árvore. Cuidado com os CPFs iguais: vocês devem pensar em uma
+		maneira eficiente de armazenar os CPFs iguais
+	
+	15) Faça a pesquisa na ABB, usando os 400 registros fornecidos pela professora,
+		gerando arquivos no mesmo modelo do item (3).
+
+	16) Repita 4 vezes os processos 14 e 15
+	
+	17) Termine de contar o tempo, faça uma média e armazene este resultado
+*/
+
+public class ArvoreABB {
+	
+	private NoArvore raiz;
+	public ArvoreABB(){
+		this.raiz = null;
+	}
+	
+	public boolean pesquisa (long chave){
+		NoArvore temp;
+
+		temp = this.pesquisa (chave, this.raiz);
+		if (temp != null)
+			return true;
+		else
+			return false;
+	}
+	
+	private NoArvore pesquisa (long chave, NoArvore no){
+		 NoArvore temp;
+		 temp = no;
+
+		if (temp != null){
+			 if (chave < temp.getInfo().getChave())
+				 temp = this.pesquisa (chave, temp.getEsq());
+			 else{
+				 if (chave > temp.getInfo().getChave())
+				 temp = this.pesquisa (chave, temp.getDir());
+			 }
+		 }
+		 return temp;
+	}
+	
+	public boolean insere (Item elem){
+		boolean existe;
+		existe = this.pesquisa(elem.getChave());
+		
+		 if (existe)
+			 return false;
+		 else{
+			 this.raiz = this.insere (elem, this.raiz);
+			 return true;
+		 }
+	}
+
+	private NoArvore insere (Item elem, NoArvore no){
+		NoArvore novo;
+
+		if (no == null){
+			novo = new NoArvore(elem);
+			return novo;
+		}
+		else {
+			 if (elem.getChave() < no.getInfo().getChave()){
+				 no.setEsq(this.insere (elem, no.getEsq()));
+				 return no;
+			 }
+			 else{
+				 no.setDir(this.insere (elem, no.getDir()));
+				 return no;
+			 }
+		}
+	}
+	
+	private NoArvore remove (int chave, NoArvore arv){
+		if (arv == null)
+			return arv;
+		else{
+			 if (chave < arv.getInfo().getChave())
+				 arv.setEsq(this.remove (chave, arv.getEsq()));
+			 else
+				 if (chave > arv.getInfo(). getChave())
+					 arv.setDir(this.remove (chave, arv.getDir()));
+			 else
+				 if (arv.getDir() == null)
+					 return arv.getEsq();
+			 else
+				 if (arv.getEsq() == null)
+					 return arv.getDir();
+				 else
+					 arv.setEsq(this.arruma (arv, arv.getEsq()));
+		}
+		return arv;
+		}
+		private NoArvore arruma (NoArvore Q, NoArvore R){
+			if (R.getDir() != null)
+				R.setDir(this.arruma (Q, R.getDir()));
+			else{
+				Q.setInfo(R.getInfo());
+				R = R.getEsq();
+			}
+			return R;
+		}
+	
+	
+}
---
+++ PesquisaQuick.java
@@ -0,0 +1,26 @@
+package Trabalho;
+/*
+ ----------------------------------------- 
+   Essa classe deve implementar os numeros 7 a 12 do trabalho.
+ -----------------------------------------
+  
+  	7) Comece a contar o tempo.
+	
+	8) Carregue o vetor com um dos arquivos de registros.
+	
+	9) Use o método QuickSort para ordenar por CPF, se houver mais de um CPF igual, eles
+		serão ordenados pelo número da conta. Grave o resultado em um arquivo com o nome
+		contendo o método (QUICK)+tipo(ALEA ou ORD ou INV)+ tamanho.txt.
+	
+	10) Faça a pesquisa binária, usando os 400 registros fornecidos pela professora, gerando
+	arquivos no mesmo modelo do item (3).
+
+	11) Repita 4 vezes os processos 8 a 10
+
+	12) Termine de contar o tempo, faça uma média e armazene este resultado.
+ 
+ */
+
+public class PesquisaQuick {
+
+}

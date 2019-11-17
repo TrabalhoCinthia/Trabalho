355d79dd07ce37a7ec77dadf18c9892bbfd961f2
bruno-br <43356485+bruno-br@users.noreply.github.com>
2019-11-16 23:20:50 -0300

    Update LeCPF.java

--- Trabalho/LeCPF.java
+++ Trabalho/LeCPF.java
@@ -4,22 +4,45 @@ import java.io.*;
 import java.util.Scanner;
 
 public class LeCPF {
+	
+	public static Cliente[] cliente10000alea;
 
+	//tem que fazer a mesma coisa para os outros arquivos (MENOS O Cliente.txt)
+	public static void inicia_vetores() {
+		cliente10000alea = criaVetor("cliente10000alea.txt");
+		
+	}
+	
+	
 	public static String ler(String nomeArquivo){
-		String numeros = "";
 		Scanner scanner;
-		String arquivo = "";
+		String conteudo = "";
 		
 		try {
 			scanner = new Scanner(new FileReader("src/Arquivos/"+nomeArquivo));
 
 			while (scanner.hasNext()) {
-				arquivo += scanner.nextLine();
+				conteudo += scanner.nextLine()+"//"; // Separa clientes
 			}
 		}catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
 		
-		return arquivo;
+		return conteudo;
 	}
+	
+	private static Cliente[] criaVetor(String nomeArquivo) {
+		
+		String str = ler(nomeArquivo);
+		String[] temp = str.split("//");
+		Cliente[] vet = new Cliente[temp.length];
+		
+		for(int i=0; i < temp.length; i++) {
+			vet[i] = new Cliente(temp[i]);
+		}
+		
+		return vet;
+		
+	}
+	
 }

679532ea8cabda2bf920d128850532300dd38d1e
bruno-br <43356485+bruno-br@users.noreply.github.com>
2019-11-17 15:02:06 -0300

    Update LeCPF.java

--- Trabalho/LeCPF.java
+++ Trabalho/LeCPF.java
@@ -4,14 +4,23 @@ import java.io.*;
 import java.util.Scanner;
 
 public class LeCPF {
-	
-	public static Cliente[] cliente10000alea;
 
-	//tem que fazer a mesma coisa para os outros arquivos (MENOS O Cliente.txt)
-	public static void inicia_vetores() {
-		cliente10000alea = criaVetor("cliente10000alea.txt");
-		
-	}
+	public static long[] cliente_cpfs;
+	public static Cliente[] cliente10000alea = criaVetor("cliente10000alea.txt");
+	public static Cliente[] cliente10000inv = criaVetor("cliente10000inv.txt");
+	public static Cliente[] cliente10000ord = criaVetor("cliente10000ord.txt");
+	public static Cliente[] cliente1000alea = criaVetor("cliente1000alea.txt");
+	public static Cliente[] cliente1000inv = criaVetor("cliente1000inv.txt");
+	public static Cliente[] cliente1000ord = criaVetor("cliente1000ord.txt");
+	public static Cliente[] cliente50000alea = criaVetor("cliente50000alea.txt");
+	public static Cliente[] cliente50000inv = criaVetor("cliente50000inv.txt");
+	public static Cliente[] cliente50000ord = criaVetor("cliente50000ord.txt");
+	public static Cliente[] cliente5000alea = criaVetor("cliente5000alea.txt");
+	public static Cliente[] cliente5000inv = criaVetor("cliente5000inv.txt");
+	public static Cliente[] cliente5000ord = criaVetor("cliente5000ord.txt");
+	public static Cliente[] cliente500alea = criaVetor("cliente500alea.txt");
+	public static Cliente[] cliente500inv = criaVetor("cliente500inv.txt");
+	public static Cliente[] cliente500ord = criaVetor("cliente500ord.txt");
 	
 	
 	public static String ler(String nomeArquivo){

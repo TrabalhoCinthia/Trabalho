4989aa44f66b663506300c026c77acfe00589425
bruno-br <43356485+bruno-br@users.noreply.github.com>
2019-11-19 15:50:47 -0300

    Update LeArquivos.java

--- Trabalho/LeArquivos.java
+++ Trabalho/LeArquivos.java
@@ -6,23 +6,29 @@ import java.util.Scanner;
 public class LeArquivos {
 
 	public static long[] cliente_cpfs = criaVetorLong("Cliente.txt");
-	public static Cliente[] cliente10000alea = criaVetor("cliente10000alea.txt");
-	public static Cliente[] cliente10000inv = criaVetor("cliente10000inv.txt");
-	public static Cliente[] cliente10000ord = criaVetor("cliente10000ord.txt");
-	public static Cliente[] cliente1000alea = criaVetor("cliente1000alea.txt");
-	public static Cliente[] cliente1000inv = criaVetor("cliente1000inv.txt");
-	public static Cliente[] cliente1000ord = criaVetor("cliente1000ord.txt");
-	public static Cliente[] cliente50000alea = criaVetor("cliente50000alea.txt");
-	public static Cliente[] cliente50000inv = criaVetor("cliente50000inv.txt");
-	public static Cliente[] cliente50000ord = criaVetor("cliente50000ord.txt");
-	public static Cliente[] cliente5000alea = criaVetor("cliente5000alea.txt");
-	public static Cliente[] cliente5000inv = criaVetor("cliente5000inv.txt");
-	public static Cliente[] cliente5000ord = criaVetor("cliente5000ord.txt");
-	public static Cliente[] cliente500alea = criaVetor("cliente500alea.txt");
-	public static Cliente[] cliente500inv = criaVetor("cliente500inv.txt");
-	public static Cliente[] cliente500ord = criaVetor("cliente500ord.txt");
+
+	public static Cliente[] cliente500ord = criaVetorOrd("cliente500ord.txt");
+	public static Cliente[] cliente500alea = criaVetorDes("cliente500alea.txt",cliente500ord);
+	public static Cliente[] cliente500inv = criaVetorDes("cliente500inv.txt",cliente500ord);
 	
+	public static Cliente[] cliente1000ord = criaVetorOrd("cliente1000ord.txt");
+	public static Cliente[] cliente1000alea = criaVetorDes("cliente1000alea.txt",cliente1000ord);
+	public static Cliente[] cliente1000inv = criaVetorDes("cliente1000inv.txt",cliente1000ord);
+
+	public static Cliente[] cliente5000ord = criaVetorOrd("cliente5000ord.txt");
+	public static Cliente[] cliente5000alea = criaVetorDes("cliente5000alea.txt",cliente5000ord);
+	public static Cliente[] cliente5000inv = criaVetorDes("cliente5000inv.txt",cliente5000ord);
 	
+	public static Cliente[] cliente10000ord = criaVetorOrd("cliente10000ord.txt");
+	public static Cliente[] cliente10000alea = criaVetorDes("cliente10000alea.txt",cliente10000ord);
+	public static Cliente[] cliente10000inv = criaVetorDes("cliente10000inv.txt",cliente10000ord);
+	
+	/*public static Cliente[] cliente50000ord = criaVetorOrd("cliente50000ord.txt");
+	public static Cliente[] cliente50000alea = criaVetorDes("cliente50000alea.txt",cliente50000ord);
+	public static Cliente[] cliente50000inv = criaVetorDes("cliente50000inv.txt",cliente50000ord);
+	*/
+	
+	//Retorna o conteudo de um arquivo dado o nome do arquivo
 	public static String ler(String nomeArquivo){
 		Scanner scanner;
 		String conteudo = "";
@@ -40,7 +46,8 @@ public class LeArquivos {
 		return conteudo;
 	}
 	
-	private static Cliente[] criaVetor(String nomeArquivo) {
+	//Cria um vetor dado o nome de um arquivo com os elementos ordenados
+	private static Cliente[] criaVetorOrd(String nomeArquivo) {
 		
 		String str = ler(nomeArquivo);
 		String[] temp = str.split("//");
@@ -48,6 +55,14 @@ public class LeArquivos {
 		
 		for(int i=0; i < temp.length; i++) {
 			vet[i] = new Cliente(temp[i]);
+			if(i>0) {
+				/*Se o valor dos cpfs forem iguais, os dois apontarão para
+				 * o mesmo objeto Cpf */
+				if(vet[i].getChave() == vet[i-1].getChave()) {
+					vet[i].setCpf(vet[i-1].getCpf());
+				}
+			}
+			
 			System.out.println("lendo "+i+" de "+temp.length);
 		}
 		
@@ -55,7 +70,31 @@ public class LeArquivos {
 		
 	}
 	
-private static long[] criaVetorLong(String nomeArquivo) {
+	/*Cria um vetor desordenado que usa os Clientes e CPFs
+	  já criados no vetor ordenado de mesmo tamanho.*/
+	private static Cliente[] criaVetorDes(String nomeArquivo, Cliente[]vet_ord) {
+		
+		String str = ler(nomeArquivo);
+		String[] temp = str.split("//");
+		Cliente[] vet = new Cliente[temp.length];
+		
+		for(int i=0; i < temp.length; i++) {
+			//cria um novo Cliente com os atributos do arquivo
+			vet[i] = new Cliente(temp[i]);
+			
+			//encontra o Cliente com o mesmo cpf e num no vet_ord e insere no vetor desordenado
+			vet[i] = PesquisaBinaria.pesqCliente(vet[i], vet_ord);
+			
+			System.out.println("lendo "+i+" de "+temp.length);
+		}
+		
+		return vet;
+		
+	}
+	
+	
+	
+	private static long[] criaVetorLong(String nomeArquivo) {
 		
 		String str = ler(nomeArquivo);
 		String[] temp = str.split("//");

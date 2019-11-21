a4c646c46899fdbf9c7dcf63f56c7b0cac65178d
bruno-br <43356485+bruno-br@users.noreply.github.com>
2019-11-20 22:57:46 -0300

    Adicionado o método pesqCpfMultp

--- Trabalho/PesquisaBinaria.java
+++ Trabalho/PesquisaBinaria.java
@@ -56,4 +56,50 @@ public class PesquisaBinaria {
 
             return aux;
 	}        
+	
+	//retorna todos os clientes com um cpf
+	public static Cliente[] pesqCpfMultp(long cpf, Cliente[]vet){
+	        int meio, esq, dir;
+	        esq = 0;
+	        dir = vet.length-1;
+	        Cliente[] mesmo_cpf = null;
+	        int qtd_dir=0, qtd_esq=0;
+	        
+	        while (esq <= dir){
+	            meio = (esq + dir)/2;
+	
+	            //Achou um cliente com o cpf
+	            if (cpf == vet[meio].getChave()) {
+	                System.out.println("achou cliente com o cpf"); 
+	            	
+	            	//Encontra a quantidade de clientes com mesmo cpf a direita
+	                 while( (meio+qtd_dir+1 <vet.length )&&(cpf == vet[meio+qtd_dir+1].getChave())) {
+	                	 qtd_dir++;
+	                 }
+	                 
+	               //Encontra a quantidade de clientes com mesmo cpf a esquerda
+	                 while( (meio-qtd_esq-1 >= 0)&&(cpf==vet[meio-qtd_esq-1].getChave()) ) {
+	                	 qtd_esq++;
+	                 }
+	                 
+	                 //Cria vetor com todos os clientes com mesmo cpf
+	                 mesmo_cpf = new Cliente[ qtd_esq + qtd_dir + 1 ];
+	                 for(int i=0; i<mesmo_cpf.length;i++) {
+	                	 mesmo_cpf[i] = vet[meio-qtd_esq+i];
+	                 }
+	            	
+	            	break;
+	            	
+	            } else{
+	                if (cpf < vet[meio].getChave()) {
+	                    dir = meio - 1;
+	                }else {
+	                    esq = meio + 1;
+	                }
+	            }
+	        }
+	
+	        return mesmo_cpf;
+	}    
+	
 }
